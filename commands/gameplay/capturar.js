55aa30cb9d31afbda0caf003ac36471f635048cd
Sn4red <joaquin.arriaga08@gmail.com>
2023-10-09 22:43:03 -0500

    Se corrigieron algunos errores de mensaje en el comando de capturar y se completó la función de actualización/registro en el comando tarjeta

--- commands/gameplay/capturar.js
+++ commands/gameplay/capturar.js
@@ -30,136 +30,132 @@ module.exports = {
             'Miembro del Consejo O5': 10000,
         };
 
-        try {
-            // Avisa a la API de Discord que la interacción se recibió correctamente y da un tiempo máximo de 15 minutos.
-            await interaction.deferReply();
 
-            const idUsuario = interaction.user.id;
-            const queryUsuario = database.collection('usuario').doc(idUsuario);
-            const snapshotUsuario = await queryUsuario.get();
+        // Avisa a la API de Discord que la interacción se recibió correctamente y da un tiempo máximo de 15 minutos.
+        await interaction.deferReply();
 
-            if (snapshotUsuario.exists) {
-                // Clase obtenida mediante probabilidad.
-                const claseObtenida = probabilidadClase();
-                // La subcolección tiene el mismo nombre que el documento que la contiene, pero es completamente en minúscula.
-                const subColeccion = claseObtenida.charAt(0).toLowerCase() + claseObtenida.slice(1);
+        // Se realiza la consulta a la base de datos.
+        const queryUsuario = database.collection('usuario').doc(interaction.user.id);
+        const snapshotUsuario = await queryUsuario.get();
+
+        if (snapshotUsuario.exists) {
+            // Clase obtenida mediante probabilidad.
+            const claseObtenida = probabilidadClase();
+            // La subcolección tiene el mismo nombre que el documento que la contiene, pero es completamente en minúscula.
+            const subColeccion = claseObtenida.charAt(0).toLowerCase() + claseObtenida.slice(1);
     
-                // Obtiene todas las cartas de SCP de la clase obtenida por probabilidad.
-                const cartas = database.collection('carta').doc(claseObtenida).collection(subColeccion);
-                const snapshotCarta = await cartas.get();
+            // Obtiene todas las cartas de SCP de la clase obtenida por probabilidad.
+            const cartas = database.collection('carta').doc(claseObtenida).collection(subColeccion);
+            const snapshotCarta = await cartas.get();
     
-                if (!snapshotCarta.empty) {
-                    // Pasa los documentos del objeto QuerySnapshot en un array.
-                    const cartasArray = snapshotCarta.docs.map(x => ({ id: x.id, data: x.data() }));
+            if (!snapshotCarta.empty) {
+                // Pasa los documentos del objeto QuerySnapshot en un array.
+                const cartasArray = snapshotCarta.docs.map(x => ({ id: x.id, data: x.data() }));
     
-                    // Mediante el objeto Math, se obtiene un índice aleatorio en base a la cantidad de cartas que hay en el array,
-                    // y se selecciona una carta aleatoria.
-                    const indiceAleatorio = Math.floor(Math.random() * cartasArray.length);
-                    const cartaAleatoria = cartasArray[indiceAleatorio];
+                // Mediante el objeto Math, se obtiene un índice aleatorio en base a la cantidad de cartas que hay en el array,
+                // y se selecciona una carta aleatoria.
+                const indiceAleatorio = Math.floor(Math.random() * cartasArray.length);
+                const cartaAleatoria = cartasArray[indiceAleatorio];
     
-                    // Datos de la carta.
-                    const idCarta = cartaAleatoria.id;
-                    const clase = claseObtenida;
-                    const archivo = cartaAleatoria.data.archivo;
-                    const nombre = cartaAleatoria.data.nombre;
+                // Datos de la carta.
+                const idCarta = cartaAleatoria.id;
+                const clase = claseObtenida;
+                const archivo = cartaAleatoria.data.archivo;
+                const nombre = cartaAleatoria.data.nombre;
     
-                    const imagePath = path.join(__dirname, `../../images/scp/${idCarta}.jpg`);
+                const imagePath = path.join(__dirname, `../../images/scp/${idCarta}.jpg`);
     
-                    // Para que todas las imágenes tengan el mismo tamaño,
-                    // se redimensionan a 300x200 píxeles.
-                    const cartaEmbed = new EmbedBuilder()
-                    .setColor(0x000000)
-                    .setTitle(`Ítem #: ${idCarta} / ${nombre}`)
-                    .setDescription(`+${xp[clase]} XP`)
-                    .addFields(
-                        { name: 'Clase', value: clase, inline: true },
-                        { name: 'Archivo', value: archivo, inline: true },
-                    )
-                    .setImage(`attachment://${idCarta}.jpg`)
-                    .setTimestamp()
-                    .setFooter({ text: 'X tiros restantes.' });
+                // Para que todas las imágenes tengan el mismo tamaño,
+                // se redimensionan a 300x200 píxeles.
+                const cartaEmbed = new EmbedBuilder()
+                .setColor(0x000000)
+                .setTitle(`Ítem #: ${idCarta} / ${nombre}`)
+                .setDescription(`+${xp[clase]} XP`)
+                .addFields(
+                    { name: 'Clase', value: clase, inline: true },
+                    { name: 'Archivo', value: archivo, inline: true },
+                )
+                .setImage(`attachment://${idCarta}.jpg`)
+                .setTimestamp()
+                .setFooter({ text: 'X tiros restantes.' });
                     
-                    // Se inserta el registro de la obtención de la carta.
-                    const registroObtencion = database.collection('obtencion').doc();
+                // Se inserta el registro de la obtención de la carta.
+                const registroObtencion = database.collection('obtencion').doc();
     
-                    await registroObtencion.set({
-                        carta: database.collection('carta').doc(claseObtenida).collection(subColeccion).doc(idCarta),
-                        usuario: queryUsuario,
-                    });
+                await registroObtencion.set({
+                    carta: database.collection('carta').doc(claseObtenida).collection(subColeccion).doc(idCarta),
+                    usuario: queryUsuario,
+                });
 
-                    // Acá se realiza la promoción de rango y nivel (si fuera el caso).
-                    const xpGanado = xp[clase];
-                    const usuario = snapshotUsuario.data();
-                    const xpMaximo = xpJugador[usuario.rango];
+                // Acá se realiza la promoción de rango y nivel (si fuera el caso).
+                const xpGanado = xp[clase];
+                const usuario = snapshotUsuario.data();
+                const xpMaximo = xpJugador[usuario.rango];
 
-                    // La variable determina qué tipo de promoción va a hacerse (rango o nivel),
-                    // para que salga diferente tipo de mensaje.
-                    let tipoPromocion = 'no';
+                // La variable determina qué tipo de promoción va a hacerse (rango o nivel),
+                // para que salga diferente tipo de mensaje.
+                let tipoPromocion = 'no';
 
-                    // Esta sección obtiene el siguiente rango en base al rango actual del usuario. Si el rango actual es
-                    // 'Miembro del Consejo O5', no hay promoción.
-                    const rangos = [
-                        'Clase D',
-                        'Oficial de Seguridad',
-                        'Investigador',
-                        'Especialista de Contención',
-                        'Agente de Campo',
-                        'Director de Sede',
-                        'Miembro del Consejo O5',
-                    ];
+                // Esta sección obtiene el siguiente rango en base al rango actual del usuario. Si el rango actual es
+                // 'Miembro del Consejo O5', no hay promoción.
+                const rangos = [
+                    'Clase D',
+                    'Oficial de Seguridad',
+                    'Investigador',
+                    'Especialista de Contención',
+                    'Agente de Campo',
+                    'Director de Sede',
+                    'Miembro del Consejo O5',
+                ];
 
-                    let indiceElementoActual = rangos.indexOf(usuario.rango);
-                    indiceElementoActual++;
+                let indiceElementoActual = rangos.indexOf(usuario.rango);
+                indiceElementoActual++;
 
-                    if (indiceElementoActual == 6) {
-                        indiceElementoActual--;
-                    }
+                if (indiceElementoActual == 6) {
+                    indiceElementoActual--;
+                }
 
-                    if ((usuario.xp + xpGanado) >= xpMaximo) {
-                        if (usuario.nivel < 20) {
-                            tipoPromocion = 'nivel';
+                if ((usuario.xp + xpGanado) >= xpMaximo) {
+                    if (usuario.nivel < 20) {
+                        tipoPromocion = 'nivel';
 
-                            await queryUsuario.update({
-                                nivel: ++usuario.nivel,
-                                xp: (usuario.xp + xpGanado) - xpMaximo,
-                            });
-                        } else {
-                            tipoPromocion = 'rango';
-
-                            await queryUsuario.update({
-                                rango: rangos[indiceElementoActual],
-                                nivel: 1,
-                                xp: (usuario.xp + xpGanado) - xpMaximo,
-                            });
-                        }
-                    } else {
                         await queryUsuario.update({
-                            xp: firebase.firestore.FieldValue.increment(xpGanado),
+                            nivel: ++usuario.nivel,
+                            xp: (usuario.xp + xpGanado) - xpMaximo,
                         });
-                    }                    
+                    } else {
+                        tipoPromocion = 'rango';
 
-                    await interaction.editReply({
-                        embeds: [cartaEmbed],
-                        files: [imagePath],
-                    });
-
-                    switch (tipoPromocion) {
-                        case 'nivel':
-                            await interaction.followUp('Felicidades ' + usuario.nick + '. Ahora eres nivel ' + usuario.nivel + '.');
-                            break;
-                        case 'rango':
-                            await interaction.followUp('Felicidades ' + usuario.nick + '. Has ascendido a ' + rangos[indiceElementoActual] + '.');
-                            break;
+                        await queryUsuario.update({
+                            rango: rangos[indiceElementoActual],
+                            nivel: 1,
+                            xp: (usuario.xp + xpGanado) - xpMaximo,
+                        });
                     }
                 } else {
-                    await interaction.reply('Error al intentar capturar un SCP. Inténtalo más tarde.');
+                    await queryUsuario.update({
+                        xp: firebase.firestore.FieldValue.increment(xpGanado),
+                    });
+                }                    
+
+                await interaction.editReply({
+                    embeds: [cartaEmbed],
+                    files: [imagePath],
+                });
+
+                switch (tipoPromocion) {
+                    case 'nivel':
+                        await interaction.followUp('Felicidades ' + usuario.nick + '. Ahora eres nivel ' + usuario.nivel + '.');
+                        break;
+                    case 'rango':
+                        await interaction.followUp('Felicidades ' + usuario.nick + '. Has ascendido a ' + rangos[indiceElementoActual] + '.');
+                        break;
                 }
             } else {
-                await interaction.reply('¡No estás registrado! Usa /tarjeta para guardar tus datos.');
+                await interaction.editReply('Error al intentar capturar un SCP. Inténtalo más tarde.');
             }
-        } catch (error) {
-            console.error('Error durante la ejecución del comando "capturar": ', error);
-            await interaction.reply('Ocurrió un error al ejecutar el comando. Por favor, inténtalo más tarde.');
+        } else {
+            await interaction.editReply('¡No estás registrado! Usa /tarjeta para guardar tus datos.');
         }
     },
 };
--- commands/information/tarjeta.js
+++ commands/information/tarjeta.js
@@ -23,159 +23,192 @@ module.exports = {
         await interaction.deferReply();
 
         // Se realiza la consulta a la base de datos.
-        const query = database.collection('usuario').where(firebase.firestore.FieldPath.documentId(), '==', interaction.user.id);
-        const snapshot = await query.get();
+        const queryUsuario = database.collection('usuario').doc(interaction.user.id);
+        const snapshotUsuario = await queryUsuario.get();
 
-        if (!snapshot.empty) {
-            const documento = snapshot.docs[0];
-            const jugador = documento.data();
+        // Si el usuario existe, se actualizan sus datos, en este caso el nickname (por si se lo cambió manualmente).
+        if (snapshotUsuario.exists) {
+            await queryUsuario.update({
+                nick: interaction.user.username,
+            });
 
-            // Datos del jugador.
-            const fecha = jugador.fecha;
-            const id = documento.id;
-            const nick = jugador.nick;
-            const nivel = jugador.nivel;
-            const rango = jugador.rango;
-            const xp = jugador.xp.toString();
+            const documento = snapshotUsuario.data();
+            const idUsuario = documento.id;
 
-            // Crea un lienzo de 450x250 píxeles y obtiene su contexto.
-            // El contexto será usado para poder modificar el lienzo.
-            const canvas = Canvas.createCanvas(450, 250);
-            const context = canvas.getContext('2d');
-
-            // Carga la imagen de fondo al lienzo y usa las dimensiones de esta para estirarlo.
-            const background = await Canvas.loadImage('./images/tarjeta/background-card.jpg');
-            context.drawImage(background, 0, 0, canvas.width, canvas.height);
-
-            // Borde de la tarjeta.
-            context.strokeStyle = '#000000';
-            context.lineWidth = 10;
-            context.strokeRect(0, 0, canvas.width, canvas.height);
-        
-            // Borde de la foto del jugador.
-            context.strokeStyle = '#FFFFFF';
-            context.lineWidth = 5;
-            context.strokeRect(25, 55, 100, 100);
-
-            // Encabezado.
-            context.font = 'bold 15px Roboto Condensed';
-            context.fillStyle = '#FFFFFF';
-            context.fillText('FUNDACIÓN SCP', 178, 28);
-
-            // Nombre en clave.
-            context.font = 'bold 16px Roboto Condensed';
-            context.fillStyle = '#FFFFFF';
-            context.fillText('Agente:', 145, 65);
-
-            context.font = 'bold 14px Roboto Condensed';
-            context.fillText(nick, 202, 65);
-        
-            // Rango.
-            context.font = 'bold 16px Roboto Condensed';
-            context.fillStyle = '#FFFFFF';
-            context.fillText('Rango:', 145, 91);
-
-            context.font = 'bold 14px Roboto Condensed';
-            context.fillText(rango, 197, 91);
-
-            // Fecha de emisión.
-            context.font = 'bold 10px Roboto Condensed';
-            context.fillStyle = '#FFFFFF';
-            context.fillText('Fecha de emisión:', 145, 117);
-            context.fillText(fecha, 220, 117);
-
-            // SCP's capturados.
-            context.font = 'bold 10px Roboto Condensed';
-            context.fillStyle = '#FFFFFF';
-            context.fillText('SCP\'s capturados:', 145, 132);
-            context.fillText('0', 220, 132);
-
-            // Etiqueta de clasificado.
-            context.font = '13px Roboto Condensed';
-            context.fillStyle = '#FF0000';
-            context.fillText('[ Clasificado ]', 41, 28);
-
-            // ID del jugador.
-            context.font = 'bold 8px Roboto Condensed';
-            context.fillStyle = '#FFFFFF';
-            context.fillText(id, 367, 98);
-
-            // Barra de progreso del jugador.
-            context.fillStyle = '#1A1A1A';
-            context.fillRect(22, 210, 406, 15);
-
-            // Barra de llenado de progreso del jugador.
-            const gradient = context.createLinearGradient(0, 213, 0, 222);
-            gradient.addColorStop(0, '#AEE064');
-            gradient.addColorStop(0.5, '#2E6C1F');
-            context.fillStyle = gradient;
-
-            // Llenado del progreso según el rango.
-            const factoresMultiplicacion = {
-                'Clase D': 8,
-                'Oficial de Seguridad': 4,
-                'Investigador': 1.6,
-                'Especialista de Contención': 0.8,
-                'Agente de Campo': 0.266,
-                'Director de Sede': 0.08,
-                'Miembro del Consejo O5': 0.04,
-            };
-
-            context.fillRect(25, 213, xp * factoresMultiplicacion[rango], 9);
-
-            // Nivel del jugador.
-            context.font = 'bold 10px Roboto Condensed';
-            context.fillStyle = '#FFFFFF';
-            context.fillText(`Nivel: ${nivel}`, 25, 203);
-
-            // XP del jugador.
-            context.font = 'bold 10px Roboto Condensed';
-            context.fillStyle = '#FFFFFF';
-
-            const textWidth = context.measureText(xp).width;
-            const xPosition = 22 + (406 - textWidth) / 2;
-
-            context.fillText(xp + ' XP', xPosition, 203);
-
-            // Nivel siguiente del jugador.
-            context.font = 'bold 10px Roboto Condensed';
-            context.fillStyle = '#FFFFFF';
-
-            let siguienteNivel = nivel;
-            siguienteNivel++;
-
-            if (siguienteNivel == 21) {
-                siguienteNivel = 'Rank Up!';
-            }
-            
-            if (siguienteNivel.length > 2) {
-                context.fillText(`Sgte. Nivel: ${siguienteNivel}`, 340, 203);
-            } else {
-                context.fillText(`Sgte. Nivel: ${siguienteNivel}`, 368, 203);
-            }
-
-            // Usando undici para realizar las solicitudes HTTP con un mejor rendimiento.
-            // Cargando la foto del jugador.
-            const { body: avatarBody } = await request(interaction.user.displayAvatarURL({ extension: 'jpg' }));
-            const avatar = await Canvas.loadImage(await avatarBody.arrayBuffer());
-
-            // Cargando el logo.
-            const logo = await Canvas.loadImage('./images/tarjeta/scp-logo-card.png');
-        
-            // Cargando el código QR DataMatrix
-            const qr = await Canvas.loadImage('./images/tarjeta/qr-datamatrix-card.png');
-        
-            // Dibuja las imágenes en el lienzo.
-            context.drawImage(avatar, 25, 55, 100, 100);
-            context.drawImage(logo, 307, 110, 70, 70);
-            context.drawImage(qr, 365, 5, 80, 80);
-        
             // Usa la clase AttachmentBuilder para que se procese el archivo y pueda adjuntarse en el reply.
-            const attachment = new AttachmentBuilder(await canvas.encode('png'), { name: 'profile-image.png' });
+            const attachment = await desplegarTarjeta(documento, idUsuario, interaction);
 
             await interaction.editReply({ files: [attachment] });
         } else {
-            await interaction.reply('¡No estás registrado! Captura un SCP para guardar tus datos.');
+            // Si el usuario no existe, se crea un documento nuevo antes de desplegar la tarjeta.
+
+            // Prepara el formato de fecha en DD/MM/YYYY.
+            const fechaActual = new Date();
+
+            const dia = ('0' + fechaActual.getDate()).slice(-2);
+            const mes = ('0' + (fechaActual.getMonth() + 1)).slice(-2);
+            const anio = fechaActual.getFullYear();
+
+            const usuarioNuevo = {
+                fecha: dia + '/' + mes + '/' + anio,
+                nick: interaction.user.username,
+                nivel: 1,
+                rango: 'Clase D',
+                xp: 0,
+            };
+
+            await database.collection('usuario').doc(interaction.user.id).set(usuarioNuevo);
+
+            // Usa la clase AttachmentBuilder para que se procese el archivo y pueda adjuntarse en el reply.
+            const attachment = await desplegarTarjeta(usuarioNuevo, interaction.user.id, interaction);
+
+            await interaction.editReply({ files: [attachment] });
+            
+            await interaction.followUp('¡Usuario nuevo! Ya puedes usar los comandos para coleccionar cartas y demás.');
         }
     },
 };
+
+async function desplegarTarjeta(documento, idUsuario, interaction) {
+    // Datos del usuario.
+    const fecha = documento.fecha;
+    const id = idUsuario;
+    const nick = documento.nick;
+    const nivel = documento.nivel;
+    const rango = documento.rango;
+    const xp = documento.xp.toString();
+    
+    // Crea un lienzo de 450x250 píxeles y obtiene su contexto.
+    // El contexto será usado para poder modificar el lienzo.
+    const canvas = Canvas.createCanvas(450, 250);
+    const context = canvas.getContext('2d');
+    
+    // Carga la imagen de fondo al lienzo y usa las dimensiones de esta para estirarlo.
+    const background = await Canvas.loadImage('./images/tarjeta/background-card.jpg');
+    context.drawImage(background, 0, 0, canvas.width, canvas.height);
+    
+    // Borde de la tarjeta.
+    context.strokeStyle = '#000000';
+    context.lineWidth = 10;
+    context.strokeRect(0, 0, canvas.width, canvas.height);
+    
+    // Borde de la foto del usuario.
+    context.strokeStyle = '#FFFFFF';
+    context.lineWidth = 5;
+    context.strokeRect(25, 55, 100, 100);
+    
+    // Encabezado.
+    context.font = 'bold 15px Roboto Condensed';
+    context.fillStyle = '#FFFFFF';
+    context.fillText('FUNDACIÓN SCP', 178, 28);
+    
+    // Nombre en clave.
+    context.font = 'bold 16px Roboto Condensed';
+    context.fillStyle = '#FFFFFF';
+    context.fillText('Agente:', 145, 65);
+    
+    context.font = 'bold 14px Roboto Condensed';
+    context.fillText(nick, 202, 65);
+    
+    // Rango.
+    context.font = 'bold 16px Roboto Condensed';
+    context.fillStyle = '#FFFFFF';
+    context.fillText('Rango:', 145, 91);
+    
+    context.font = 'bold 14px Roboto Condensed';
+    context.fillText(rango, 197, 91);
+    
+    // Fecha de emisión.
+    context.font = 'bold 10px Roboto Condensed';
+    context.fillStyle = '#FFFFFF';
+    context.fillText('Fecha de emisión:', 145, 117);
+    context.fillText(fecha, 220, 117);
+    
+    // SCP's capturados.
+    context.font = 'bold 10px Roboto Condensed';
+    context.fillStyle = '#FFFFFF';
+    context.fillText('SCP\'s capturados:', 145, 132);
+    context.fillText('0', 220, 132);
+    
+    // Etiqueta de clasificado.
+    context.font = '13px Roboto Condensed';
+    context.fillStyle = '#FF0000';
+    context.fillText('[ Clasificado ]', 41, 28);
+    
+    // ID del usuario.
+    context.font = 'bold 8px Roboto Condensed';
+    context.fillStyle = '#FFFFFF';
+    context.fillText(id, 367, 98);
+    
+    // Barra de progreso del usuario.
+    context.fillStyle = '#1A1A1A';
+    context.fillRect(22, 210, 406, 15);
+    
+    // Barra de llenado de progreso del usuario.
+    const gradient = context.createLinearGradient(0, 213, 0, 222);
+    gradient.addColorStop(0, '#AEE064');
+    gradient.addColorStop(0.5, '#2E6C1F');
+    context.fillStyle = gradient;
+    
+    // Llenado del progreso según el rango.
+    const factoresMultiplicacion = {
+        'Clase D': 8,
+        'Oficial de Seguridad': 4,
+        'Investigador': 1.6,
+        'Especialista de Contención': 0.8,
+        'Agente de Campo': 0.266,
+        'Director de Sede': 0.08,
+        'Miembro del Consejo O5': 0.04,
+    };
+    
+    context.fillRect(25, 213, xp * factoresMultiplicacion[rango], 9);
+    
+    // Nivel del usuario.
+    context.font = 'bold 10px Roboto Condensed';
+    context.fillStyle = '#FFFFFF';
+    context.fillText(`Nivel: ${nivel}`, 25, 203);
+    
+    // XP del usuario.
+    context.font = 'bold 10px Roboto Condensed';
+    context.fillStyle = '#FFFFFF';
+    
+    const textWidth = context.measureText(xp).width;
+    const xPosition = 22 + (406 - textWidth) / 2;
+    
+    context.fillText(xp + ' XP', xPosition, 203);
+    
+    // Nivel siguiente del usuario.
+    context.font = 'bold 10px Roboto Condensed';
+    context.fillStyle = '#FFFFFF';
+    
+    let siguienteNivel = nivel;
+    siguienteNivel++;
+    
+    if (siguienteNivel == 21) {
+        siguienteNivel = 'Rank Up!';
+    }
+        
+    if (siguienteNivel.length > 2) {
+        context.fillText(`Sgte. Nivel: ${siguienteNivel}`, 340, 203);
+    } else {
+        context.fillText(`Sgte. Nivel: ${siguienteNivel}`, 368, 203);
+    }
+    
+    // Usando undici para realizar las solicitudes HTTP con un mejor rendimiento.
+    // Cargando la foto del usuario.
+    const { body: avatarBody } = await request(interaction.user.displayAvatarURL({ extension: 'jpg' }));
+    const avatar = await Canvas.loadImage(await avatarBody.arrayBuffer());
+    
+    // Cargando el logo.
+    const logo = await Canvas.loadImage('./images/tarjeta/scp-logo-card.png');
+    
+    // Cargando el código QR DataMatrix
+    const qr = await Canvas.loadImage('./images/tarjeta/qr-datamatrix-card.png');
+    
+    // Dibuja las imágenes en el lienzo.
+    context.drawImage(avatar, 25, 55, 100, 100);
+    context.drawImage(logo, 307, 110, 70, 70);
+    context.drawImage(qr, 365, 5, 80, 80);
+    
+    return new AttachmentBuilder(await canvas.encode('png'), { name: 'profile-image.png' });
+}

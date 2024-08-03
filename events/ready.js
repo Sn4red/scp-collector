481dcb9a7826a55e97abb75f9160ca42f372b578
Sn4red <joaquin.arriaga08@gmail.com>
2024-08-02 23:49:08 -0400

    Message in console edited when bot is live.

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,6 @@ module.exports = {
     name: Events.ClientReady,
     once: true,
     execute(client) {
-        console.log(`Functional bot ${client.user.tag}`);
+        console.log(`${new Date()} >>> *** Functional bot ${client.user.tag} ***`);
     },
 };

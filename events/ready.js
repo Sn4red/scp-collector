8cf006a57f7f7fe464cf24dda7dd2c36726d0be8
Sn4red <joaquin.arriaga08@gmail.com>
2024-08-08 21:27:53 -0400

    Unnecesary comment.

--- events/ready.js
+++ events/ready.js
@@ -4,7 +4,6 @@ module.exports = {
     name: Events.ClientReady,
     once: true,
     execute(client) {
-        // TODO: mejorar el mensaje de consola, para que sea mas llamativo y facil de encontrar en la terminal.
         console.log(`${new Date()} >>> *** Functional bot ${client.user.tag} ***`);
 
         console.log('\n ,---.   ,-----.,------.      ,-----.       ,--.,--.               ,--.                 ');

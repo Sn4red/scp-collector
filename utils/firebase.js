57e88543a1cc0e6bd3231feb50d62e9f8d076ca9
Sn4red <joaquin.arriaga08@gmail.com>
2025-06-10 21:44:14 -0400

    Parse Firebase key from the .env file
    Instead of using the Firebase key JSON file directly in the project
    directory, this change reads from the .env file, and it is parsed from
    string to an object. This way it will be easier to use external tools
    in the production environment to bring the JSON key to the code.

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,6 +1,16 @@
 const firebase = require('firebase-admin');
-const serviceAccount = require(process.env.FIREBASE_PRIVATE_KEY);
 
+// * Replaces every real newline character '\n' with the escaped version '\\n'
+// * to avoid issues with the parsing.
+const safeString = process.env.FIREBASE_PRIVATE_KEY.replace(/\n/g, '\\n');
+
+// * Parses the stringified JSON to a real JSON object.
+const serviceAccount = JSON.parse(safeString);
+
+// * Replaces the escaped newline characters '\\n' back to real newlines '\n'.
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+
+// * Initializes the Firebase Admin SDK with the service account credentials.
 firebase.initializeApp({
     credential: firebase.credential.cert(serviceAccount),
     databaseURL: process.env.FIREBASE_DATABASE_URL,

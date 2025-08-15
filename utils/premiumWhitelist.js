63fa3ecd9be46589f04bb9030e24603088aaf266
Sn4red <joaquin.arriaga08@gmail.com>
2025-08-15 19:28:56 -0400

    Add user to the premium white list
    A tester was added to the premium white list.

--- utils/premiumWhitelist.js
+++ utils/premiumWhitelist.js
@@ -1,6 +1,7 @@
 const premiumWhitelist = [
-    '402580354936078336', // * sn4red
-    '486626515916816396', // * abyss7348
+    '402580354936078336', // * sn4red (owner)
+    '486626515916816396', // * abyss7348 (moderator)
+    '541471927894671370', // * kanekisad (tester)
 ];
 
 module.exports = premiumWhitelist;
